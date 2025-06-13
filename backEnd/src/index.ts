f093ebd72a73bbcf3322f3f966b4ae80db07039c
Matt2130 <manuel.hdz.rubio07@gmail.com>
2025-06-13 17:40:15 -0600

    connect front with back - Index

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -3,10 +3,15 @@ import morgan from 'morgan';
 import authRoutes from './routes/auth.routes';
 import connectDB from './config/db';
 import 'dotenv/config';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT_SERVER as string;
 
+app.use(cors({
+  origin: process.env.URL_FRONT, // <-- Ajusta a donde corre tu frontend
+  credentials: true, // si usas cookies o headers de autenticación
+}));
 app.use(express.json())
 app.use(morgan('dev'));
 //Morgan sirve para ver los logs de las peticiones que hagamos

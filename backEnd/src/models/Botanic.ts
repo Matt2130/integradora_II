41a53bed0f63dad6b1f2f5c9680a77aafde3cb54
Matt2130 <manuel.hdz.rubio07@gmail.com>
2025-06-18 14:02:28 -0600

    botanic model

---
+++ backEnd/src/models/Botanic.ts
@@ -0,0 +1,65 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IPlant {
+  plantName: string;
+  type: string;
+  status: 'Growing' | 'Geerntet' | 'Dead';
+  plantationDate: Date;
+}
+
+export interface IBotanicInventory extends Document {
+  name: string;
+  ubication: string;
+  plants: IPlant[];
+  status: boolean;
+  createDate: Date;
+}
+
+const PlantSchema: Schema = new Schema<IPlant>({
+    plantName: { 
+        type: String, 
+        required: true 
+    },
+    type: { 
+        type: String, 
+        required: true 
+    },
+    status: { 
+        type: String, 
+        enum: ['Growing', 'Geerntet', 'Dead'], 
+        required: true 
+    },
+    plantationDate: { 
+        type: Date, 
+        required: true 
+    },
+},
+    { versionKey: false });
+
+const BotanicInventorySchema = new Schema<IBotanicInventory>({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    ubication: { 
+        type: String, 
+        required: true 
+    },
+    plants: {
+        type: [PlantSchema],
+        required: true,
+        validate: [(array: string | any[]) => array.length > 0, 'Debe contener al menos una planta']
+    },
+    status: { 
+        type: Boolean, 
+        default: true 
+    },
+    createDate: { 
+        type: Date, 
+        default: Date.now 
+    }
+},
+    { versionKey: false });
+
+export default mongoose.model<IBotanicInventory>('BotanicInventory', BotanicInventorySchema);
